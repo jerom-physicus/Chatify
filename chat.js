50c04d86481c277796306c7fbe6fcd24a04ca0cf
jeremiah <114723077+jerom-physicus@users.noreply.github.com>
2023-06-24 23:51:44 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -18,9 +18,12 @@ const app = initializeApp(firebaseConfig);
 var db = getDatabase(app);
 const data = localStorage.getItem('room_data');
 const roomtype = localStorage.getItem('room_type');
+const username = localStorage.getItem('username');
+var email = username.replace(' ','')
+
 
 var email1 = localStorage.getItem('email');
-var email = email1.replace('@gmail.com','')
+//var email = email1.replace('@gmail.com','')
 const room_name = localStorage.getItem('room_name');
 document.getElementById("room_title").innerHTML = room_name
 
@@ -41,12 +44,10 @@ if(roomtype =='room'){
       let string =  values[i]
       var lastIndex = string.lastIndexOf(" ");
       var str = string.substring(0, lastIndex);
-      console.log(str)
 
       const filter = string.split(' ');
       const j =filter.length-1
       let username = filter[j]
-      console.log(filter[j])
       
       
      // str = lastIndex.substring(0, lastIndex);
@@ -60,6 +61,7 @@ else{
     chat.innerHTML = ""
     let values = Object.values(snapshot.val())
     let setdata = Object.entries(snapshot.val())
+    let keys = Object.keys(snapshot.val())
     let values_length = values.length
     let sorted_value = values_length-3
   
@@ -67,8 +69,22 @@ else{
     for (let i=0 ; i <sorted_value; i++){
       let itemsarray = setdata[i]
       let room_name =itemsarray[1]
+      //let filter = values.length
+      var lastIndex = values.lastIndexOf(" ");
+      let string =  values[i]
+      var lastIndex = string.lastIndexOf(" ");
+      var str = string.substring(0, lastIndex);
+
+      const filter = string.split(' ');
+      const j =filter.length-1
+      let username = filter[j]
       
-      appendListChatElement(values[i])
+      
+     // str = lastIndex.substring(0, lastIndex);
+ 
+      appendListChatElement2(str,username,keys[i])
+      
+      
     }
 })
 
@@ -86,9 +102,23 @@ function appendListChatElement(values,username,keys){
     chatdiv.append(chat_li)
     chat_li.addEventListener('dblclick',function(){
       remove(ref(db,'rooms/'+`${room_name}/`+keys))
-      console.log(keys)
     })
     
+}
+function appendListChatElement2(values,username,keys){
+  let chat_li = document.createElement("li")
+  let chatdiv = document.createElement("div")
+  let chatname = document.createElement("p")
+  chatname.textContent = username
+  chatdiv.append(chatname)
+
+  chat_li.textContent =values
+  chat.append(chatdiv)
+  chatdiv.append(chat_li)
+  chat_li.addEventListener('dblclick',function(){
+    remove(ref(db,'rooms2/'+`${room_name}/`+keys))
+  })
+  
 }
 if(roomtype =='room'){
   document.getElementById('send-btn').addEventListener('click',function(){
@@ -103,7 +133,7 @@ else{
   document.getElementById('send-btn').addEventListener('click',function(){
     const message_chat = document.getElementById('chat_int').value
     chat_int.value = ""
-    push(ref(db,"rooms2/"+room_name),message_chat)
+    push(ref(db,"rooms2/"+room_name),message_chat+' '+email)
    
 })
 }
